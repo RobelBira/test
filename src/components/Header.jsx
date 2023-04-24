6ab2c1490f4bd41aa7b7da8ae46f6063fc2f0fe3
Robel <127315065+RobelBira@users.noreply.github.com>
2023-04-24 15:00:52 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Header() {
+  return (
+    <header>
+       <div className="container">
+        <h2>Feedback UI</h2>
+    
+            </div> 
+    </header>
+
+)
+}
+Header.defaultProps={
+textColor: '#ff6a95',
+bgColor: 'rgba(0,0,0,0.4)'
+    }
+export default Header
---
+++ src/data/FeedbackData.jsx
@@ -0,0 +1,23 @@
+
+const FeedbackData=[
+    {
+        id:1,
+        rating:10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Reprehenderit esse'
+    },
+    {
+        id:2,
+        rating:5,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Reprehenderit esse'
+    },
+    {
+        id:3,
+        rating:8,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Reprehenderit esse'
+    },
+]
+
+
+
+
+export default FeedbackData
